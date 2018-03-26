f137c5a26bfcb3d96be064a8b22c869889cd4105
andsky90 <andsky90@gmail.com>
2018-03-26 13:41:00 +0300

    Parse JSONs

--- app/src/main/java/com/i550/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/i550/photogallery/PhotoGalleryFragment.java
@@ -1,16 +1,21 @@
 package com.i550.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 public class PhotoGalleryFragment extends Fragment{
     private RecyclerView mPhotoRecyclerView;
+    private static final String TAG = "PhotoGalleryFragment";
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
@@ -20,6 +25,7 @@ public class PhotoGalleryFragment extends Fragment{
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);    // - удерживаем фрагмент;
+        new FetchItemsTask().execute();         //-запускаем асинх таск - получение данных...
     }
 
     @Nullable @Override
@@ -28,5 +34,23 @@ public class PhotoGalleryFragment extends Fragment{
         mPhotoRecyclerView = v.findViewById(R.id.photo_recycler_view);
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
         return v; // всё что выше - подготовка вью - фрагмента (надо);
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void,Void,Void>{ // создает фоновый поток и выполняет doInBackground
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            /*
+            try {
+                String result = new FlickrFetchr().getUrlString("https://www.bignerdranch.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);       //запускаем получение данных (фция во FlickrFetchr)
+            } catch (IOException ioe){
+                Log.e(TAG, "Failed to fetch URL: ", ioe);               //и логгируем получилось или нет
+            }
+            return null;
+        }*/
+            new FlickrFetchr().fetchItems();
+            return null;
+        }
     }
 }
