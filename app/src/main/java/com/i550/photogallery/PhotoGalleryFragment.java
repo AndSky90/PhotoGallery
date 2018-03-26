1b4c27bcb387d08ad906bc027126e982d51112f9
andsky90 <andsky90@gmail.com>
2018-03-26 15:18:46 +0300

    Shows data

--- app/src/main/java/com/i550/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/i550/photogallery/PhotoGalleryFragment.java
@@ -6,16 +6,19 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class PhotoGalleryFragment extends Fragment{
-    private RecyclerView mPhotoRecyclerView;
     private static final String TAG = "PhotoGalleryFragment";
+    private RecyclerView mPhotoRecyclerView;
+    private List<GalleryItem> mItems = new ArrayList<>();
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
@@ -33,13 +36,62 @@ public class PhotoGalleryFragment extends Fragment{
         View v = inflater.inflate(R.layout.fragment_photo_gallery, container,false);
         mPhotoRecyclerView = v.findViewById(R.id.photo_recycler_view);
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+        setupAdapter();
         return v; // всё что выше - подготовка вью - фрагмента (надо);
     }
 
-    private class FetchItemsTask extends AsyncTask<Void,Void,Void>{ // создает фоновый поток и выполняет doInBackground
+    private void setupAdapter() {
+        if(isAdded()) {     //если фрагмент был присоединен к активности (изза асинкТаск метод может вызваться не только из активити
+            mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));        //присоединяет адаптер
+        }       //проверяет текущее состояние модели, вызывается при любом ее изменении и при создании нового РесайклВью,
+        // настраивает адаптер для РесайклВью
+    }
+
+    private class PhotoHolder extends RecyclerView.ViewHolder{  //viewHolder  - вывод на экран
+        private TextView mTitleTextView;
+
+        public PhotoHolder(View itemView) {
+            super(itemView);
+            this.mTitleTextView = (TextView)itemView;
+        }
+        public void bindGalleryItem(GalleryItem item){
+            mTitleTextView.setText(item.toString());
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder>{   //адаптер, предоставляет необходимой ФотоХолдер на основании списка ГаллериИтем
+        private List<GalleryItem> mGalleryItems;
+
+        public PhotoAdapter(List<GalleryItem> galleryItems){
+            mGalleryItems=galleryItems;
+        }
 
         @Override
-        protected Void doInBackground(Void... voids) {
+        public PhotoHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            TextView textView = new TextView(getActivity());
+            return new PhotoHolder(textView);
+        }
+
+        @Override
+        public void onBindViewHolder(PhotoHolder holder, int position) {
+            GalleryItem galleryItem = mGalleryItems.get(position);  //получаем ГалериИтем по позиции
+            holder.bindGalleryItem(galleryItem);    //биндим
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mGalleryItems.size();
+        }
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void,Void,List<GalleryItem>>{ // создает фоновый поток и выполняет doInBackground
+        // 1 параметр - тип входных параметров для execute() которые -> в DoInBkgnd
+        // 2 параметр - тип для передачи инфы о ходе выполнения
+        // 3 параметр - тип результата АсинхТаска (ретурн бэкграунда и входной параметр онПостЕхе)
+
+        @Override
+        protected List<GalleryItem> doInBackground(Void... voids) {
             /*
             try {
                 String result = new FlickrFetchr().getUrlString("https://www.bignerdranch.com");
@@ -49,8 +101,13 @@ public class PhotoGalleryFragment extends Fragment{
             }
             return null;
         }*/
-            new FlickrFetchr().fetchItems();
-            return null;
+            return new FlickrFetchr().fetchItems();
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryItem> items) {     //выполняется после окончания доп.потока, в основном
+            mItems=items;       //после загрузки фоток обновляет итемс (загружает лист ГаллериИтемс)
+            setupAdapter();     //обновление источника данных РесайклВью
         }
     }
 }
